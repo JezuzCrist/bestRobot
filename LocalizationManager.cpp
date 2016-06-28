e6e995da05e5216375e2ca7ec89966e73bfafa6f
jezuzcrist <vfxlord@gmail.com>
2016-06-28 16:13:09 +0300

    created partical / localizationManager classes

---
+++ LocalizationManager.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "LocalizationManager.h"
+
+
+LocalizationManager::LocalizationManager(void)
+{
+}
+
+
+LocalizationManager::~LocalizationManager(void)
+{
+}
---
+++ Particle.cpp
@@ -0,0 +1,12 @@
+#include "stdafx.h"
+#include "Particle.h"
+
+
+Particle::Particle(void)
+{
+}
+
+
+Particle::~Particle(void)
+{
+}
