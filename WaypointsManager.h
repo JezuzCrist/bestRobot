9667a08233addd8e51cc08a79bf47fe219e83573
jezuzcrist <vfxlord@gmail.com>
2016-06-28 19:19:25 +0300

    waypoints manager implemented

--- WaypointsManager.h
+++ WaypointsManager.h
@@ -1,12 +1,29 @@
 #pragma once
+#include "Cell.h"
+#include <vector>
+#include <math.h>
+
+using namespace std;
+
 class WaypointsManager
 {
 public:
-	WaypointsManager(void);
+	WaypointsManager(vector<Position*> robotPath);
 	~WaypointsManager(void);
 
+	void update(Position* currentPosition);
+	bool isRobotInEndGoal();
+	Position* getActiveWaypoint();
 
+private:
+	const int static distanceThreshold = 10;
+	const int static numberOfCellsToSkipInPath = 5;
 
-
+	vector<Position*> _robotPath;
+	vector<Position*> _wayPoints;
+	unsigned wayPointIndex;
+	bool _isRobotInWayPoint(Position* currentPosition);
+	void _setNextWayPoint();
+	void _buildWayPoints();
 };
 
