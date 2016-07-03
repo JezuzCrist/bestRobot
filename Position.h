9a2d552c3c940293bec28a2566a7e90bbb6505b0
jezuzcrist <vfxlord@gmail.com>
2016-07-03 12:02:10 +0300

    yaw proprety added to position

--- Position.h
+++ Position.h
@@ -3,10 +3,16 @@
 struct Position {
 	int x;
 	int y;
-
+	double yaw;
 	Position(int x, int y)
 	{
 		this -> x = x;
 		this -> y = y;
+	}
+	Position(int x, int y,double yaw)
+	{
+		this -> x = x;
+		this -> y = y;
+		this->yaw = yaw;
 	}
 };
