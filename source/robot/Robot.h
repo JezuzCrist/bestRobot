9226c91b5875d71263853c5eea288865546b5600
jezuzcrist <vfxlord@gmail.com>
2016-07-04 20:30:40 +0300

    robot stabs

--- source/robot/Robot.h
+++ source/robot/Robot.h
@@ -1,5 +1,8 @@
 #pragma once
+#include <libplayerc++/playerc++.h>
+#include "../common/PositionD.h"
 using namespace std;
+using namespace PlayerCc;
 
 
 #define NUMBER_OF_LASERS 6
@@ -7,6 +10,20 @@ using namespace std;
 class Robot
 {
 public:
-	Robot(void);
+	Robot(char* ip, int port,PositionD* startingPosition,int width,int height);
 	~Robot(void);
+	PositionD* getPosition();
+	float getLaserDistance(int index);
+	int getWidth();
+	int getHeight();
+	void goTo(PositionD* wantedPosition);
+
+private:
+	PositionD _position;
+	int _width,_height;
+	PlayerClient* _playerClient;
+	Position2dProxy* _playerPsition;
+	LaserProxy* _lasers;
+	void _updatePosition();
+
 };
