aa2c9b268b58a6138772b1d5f588425bd0e9fc65
jezuzcrist <vfxlord@gmail.com>
2016-07-05 12:07:37 +0300

    implemented some robot functions

--- source/robot/Robot.cpp
+++ source/robot/Robot.cpp
@@ -1,23 +1,61 @@
 #include "Robot.h"
+#include <libplayerc++/playerc++.h>
+
 using namespace std;
-using namespace PlayerCc;
 
 Robot::Robot(char* ip, int port,WorldPosition3D* startingPosition,int width,int height)
 {
 	this->_playerClient = new PlayerCc::PlayerClient(ip,port);
 	this->_playerPsition = new PlayerCc::Position2dProxy(this->_playerClient);
-	this->_lasers = new LaserProxy(this->_playerClient);
+	this->_lasers = new PlayerCc::LaserProxy(this->_playerClient);
 
-	this->_playerPsition->SetMotorEnable(true);
+	this->_position = startingPosition;
+
+	this->_height = height;
+	this->_width = width;
+
+
+
 	this->_playerPsition->SetOdometry(
 		startingPosition->x,startingPosition->y,startingPosition->yaw);
+	this->_playerPsition->SetMotorEnable(true);
 
 }
-
+int Robot::getWidth(){
+	return this->_width;
+}
+int Robot::getHeight(){
+	return this->_height;
+}
 void Robot::_setSpeed(float speed, float angularSpeed){
 	this->_playerPsition->SetSpeed(speed,angularSpeed);
+}
+WorldPosition3D* Robot::getPosition(){
+	return this->_position;
+}
+float Robot::getLaserDistance(int laserIndex){
+	float laserValue = (*this->_lasers)[laserIndex];
+	return laserValue;
 }
 
 Robot::~Robot(void)
 {
+}
+
+void Robot::_setYaw(double wantedYaw){
+
+}
+bool isRobotTolaratedAtLocation(WorldPosition3D* robot, WorldPosition3D* wanted){
+	bool isTolarated = false;
+	double 	deltaX = robot->x - wanted->x,
+			deltaY = robot->y - wanted->y;
+	double distance = sqrt( pow(deltaX,2) + pow(deltaY ,2) );
+
+	return isTolarated;
+}
+void Robot::goTo(WorldPosition3D* wantedPosition){
+
+	while(isRobotTolaratedAtLocation(this->_position,wantedPosition) == false){
+		// move robot to location
+	}
 }
--- source/robot/Robot.h
+++ source/robot/Robot.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <libplayerc++/playerc++.h>
 #include<string.h>
+#include <iostream>
+#include<stdlib.h>
 #include "../common/Position.h"
 
 
@@ -13,12 +15,15 @@ public:
 	Robot(char* ip, int port,WorldPosition3D* startingPosition,int width,int height);
 	~Robot(void);
 	WorldPosition3D* getPosition();
-	float getLaserDistance(int index);
+	float getLaserDistance(int laserIndex);
 	int getWidth();
 	int getHeight();
 	void goTo(WorldPosition3D* wantedPosition);
 
 private:
+	const double static angleTolerance = 1;
+	const double static distanceTolerance = 5;
+
 	WorldPosition3D* _position;
 	int _width,_height;
 	PlayerCc::PlayerClient* _playerClient;
@@ -26,5 +31,5 @@ private:
 	PlayerCc::LaserProxy* _lasers;
 	void _updatePosition();
 	void _setSpeed(float speed, float angularSpeed);
-
+	void _setYaw(double wantedYaw);
 };
